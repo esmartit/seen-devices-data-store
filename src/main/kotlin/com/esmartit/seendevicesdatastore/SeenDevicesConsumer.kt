59ddec8ba29383a2a654d459b7a7e8732f838fe9
gus-esmartit <gustavo.liccioni@esmartit.es>
2020-03-23 07:33:31 +0100

    fix: storing in repository

--- src/main/kotlin/com/esmartit/seendevicesdatastore/SeenDevicesConsumer.kt
+++ src/main/kotlin/com/esmartit/seendevicesdatastore/SeenDevicesConsumer.kt
@@ -1,14 +1,15 @@
 package com.esmartit.seendevicesdatastore
 
+import org.slf4j.LoggerFactory
 import org.springframework.cloud.stream.annotation.EnableBinding
 import org.springframework.cloud.stream.annotation.StreamListener
 import org.springframework.cloud.stream.messaging.Sink
 
 @EnableBinding(Sink::class)
 class SeenDevicesConsumer(private val repository: DeviceStatRepository) {
-
+    private val logger = LoggerFactory.getLogger(SeenDevicesConsumer::class.java)
     @StreamListener(Sink.INPUT)
     fun handle(seenDevice: DeviceSeenEvent) {
-        repository.save(DeviceStat(seenDevice = seenDevice))
+        logger.info(seenDevice.toString())
     }
 }
