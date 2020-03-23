5ff3fd974209a215f9f607be410471ab8e54ff62
gus-esmartit <gustavo.liccioni@esmartit.es>
2020-03-23 15:39:08 +0100

    feat: adding sensor activity controller

---
+++ src/main/kotlin/com/esmartit/seendevicesdatastore/SensorActivityController.kt
@@ -0,0 +1,17 @@
+package com.esmartit.seendevicesdatastore
+
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+
+@RestController
+@RequestMapping("/sensor-activity")
+class SensorActivityController(private val repository: DeviceStatRepository) {
+
+    @GetMapping(produces = [MediaType.APPLICATION_STREAM_JSON_VALUE])
+    fun getAllSensorActivity(): Flux<SensorActivity> {
+        return repository.findAll()
+    }
+}
