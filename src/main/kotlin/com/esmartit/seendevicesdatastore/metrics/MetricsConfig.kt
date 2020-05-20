00ba13c08d04a1315c389d75d80a08ca832620c4
gus-esmartit <gustavo.liccioni@esmartit.es>
2020-05-20 21:36:46 +0200

    feat: adding registry application name

---
+++ src/main/kotlin/com/esmartit/seendevicesdatastore/metrics/MetricsConfig.kt
@@ -0,0 +1,18 @@
+package com.esmartit.seendevicesdatastore.metrics
+
+import io.micrometer.core.instrument.MeterRegistry
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class MetricsConfig {
+
+    @Bean
+    fun metricsCommonTags(): MeterRegistryCustomizer<MeterRegistry> {
+        return MeterRegistryCustomizer { registry: MeterRegistry ->
+            registry.config().commonTags("application", "seen-devices-data-store")
+        }
+    }
+}
