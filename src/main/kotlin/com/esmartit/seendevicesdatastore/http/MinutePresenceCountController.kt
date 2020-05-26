e18583e2d4d125f300c9d3e6d061f43178a9885c
gus-esmartit <gustavo.liccioni@esmartit.es>
2020-05-26 09:44:01 +0200

    fix: endpoint sequence

--- src/main/kotlin/com/esmartit/seendevicesdatastore/http/MinutePresenceCountController.kt
+++ src/main/kotlin/com/esmartit/seendevicesdatastore/http/MinutePresenceCountController.kt
@@ -10,6 +10,7 @@ import reactor.core.publisher.Flux
 import java.time.Duration
 import java.time.Instant
 import java.time.ZoneOffset
+import java.time.temporal.ChronoUnit
 import java.util.UUID
 import java.util.function.BiFunction
 
@@ -28,12 +29,16 @@ class MinutePresenceCountController(
             .groupBy { it.time }
             .flatMap { g -> g.reduce { _, u -> u } }
             .sort { o1, o2 -> o1.time.compareTo(o2.time) }
+            .filter { it.time.isBefore(getTwoMinutesAgo()) }
 
-        val ticker = Flux.interval(Duration.ofSeconds(1)).onBackpressureDrop()
+        val ticker = Flux.interval(Duration.ofSeconds(15)).onBackpressureDrop()
         val latest = repository.findWithTailableCursorBy()
+            .scan(mutableMapOf(), ::scanNewEvents)
         val currentFlux =
-            Flux.combineLatest(ticker, latest, BiFunction { _: Long, b: MinutePresenceCountTailable -> b })
-                .scan(mutableMapOf(), ::scanNewEvents)
+            Flux.combineLatest(
+                ticker,
+                latest,
+                BiFunction { _: Long, b: MutableMap<Int, MinutePresenceCountTailable> -> b })
                 .map { getTwoMinutesAgoCount(it) }
 
 
@@ -41,12 +46,14 @@ class MinutePresenceCountController(
     }
 
     private fun getTwoMinutesAgoCount(it: MutableMap<Int, MinutePresenceCountTailable>): MinutePresenceCountTailable {
-        val twoMinutesAgo = Instant.now().minus(Duration.ofMinutes(2))
+        val twoMinutesAgo = getTwoMinutesAgo()
         return it[twoMinutesAgo.atZone(ZoneOffset.UTC).minute] ?: MinutePresenceCountTailable(
-            time = twoMinutesAgo,
-            id = UUID.randomUUID().toString()
+            time = twoMinutesAgo.truncatedTo(ChronoUnit.MINUTES),
+            id = UUID.randomUUID().toString().replace("-", "").substring(0..22)
         )
     }
+
+    private fun getTwoMinutesAgo() = Instant.now().minus(Duration.ofMinutes(2))
 
     private fun scanNewEvents(
         a: MutableMap<Int, MinutePresenceCountTailable>,
