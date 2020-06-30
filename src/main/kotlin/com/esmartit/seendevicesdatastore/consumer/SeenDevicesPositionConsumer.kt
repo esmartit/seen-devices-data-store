ee3b85b9f6dcd10f72d7be1d17754303138373e4
gus-esmartit <gustavo.liccioni@esmartit.es>
2020-06-30 15:00:32 +0200

    fix: store radius events

--- src/main/kotlin/com/esmartit/seendevicesdatastore/consumer/SeenDevicesPositionConsumer.kt
+++ src/main/kotlin/com/esmartit/seendevicesdatastore/consumer/SeenDevicesPositionConsumer.kt
@@ -1,10 +1,6 @@
 package com.esmartit.seendevicesdatastore.consumer
 
 import com.esmartit.seendevicesdatastore.application.incomingevents.DeviceWithPresenceEvent
-import com.esmartit.seendevicesdatastore.repository.DevicePositionRepository
-import com.esmartit.seendevicesdatastore.repository.DeviceWithPosition
-import com.esmartit.seendevicesdatastore.repository.Position
-import com.esmartit.seendevicesdatastore.application.incomingevents.EventToSensorActivity
 import org.springframework.cloud.stream.annotation.EnableBinding
 import org.springframework.cloud.stream.annotation.Input
 import org.springframework.cloud.stream.annotation.StreamListener
@@ -12,30 +8,12 @@ import org.springframework.messaging.SubscribableChannel
 
 @EnableBinding(SeenDevicesPositionInput::class)
 class SeenDevicesPositionConsumer(
-    private val eventToSensorActivity: EventToSensorActivity,
-    private val repository: DevicePositionRepository
+    private val seenDevicesPositionService: SeenDevicesPositionService
 ) {
 
     @StreamListener(SeenDevicesPositionInput.DEVICE_POSITION_INPUT)
     fun handle(event: DeviceWithPresenceEvent) {
-
-        val incomingSensorActivity = eventToSensorActivity.convertToSensorActivity(event.deviceDetectedEvent)
-        val existingSensorActivity = repository.findByMacAddressAndSeenTime(
-            incomingSensorActivity.device.macAddress,
-            incomingSensorActivity.seenTime
-        )
-        val existingRSSI = existingSensorActivity?.position ?: Position.NO_POSITION
-
-        if (event.position.value > existingRSSI.value) {
-            repository.save(
-                DeviceWithPosition(
-                    id = existingSensorActivity?.id,
-                    macAddress = incomingSensorActivity.device.macAddress,
-                    position = event.position,
-                    seenTime = incomingSensorActivity.seenTime
-                )
-            )
-        }
+        seenDevicesPositionService.handle(event)
     }
 }
 
---
+++ src/main/kotlin/com/esmartit/seendevicesdatastore/consumer/SeenDevicesPositionService.kt
@@ -0,0 +1,42 @@
+package com.esmartit.seendevicesdatastore.consumer
+
+import com.esmartit.seendevicesdatastore.application.incomingevents.DeviceWithPresenceEvent
+import com.esmartit.seendevicesdatastore.application.incomingevents.EventToSensorActivity
+import com.esmartit.seendevicesdatastore.application.radius.online.RadiusActivityRepository
+import com.esmartit.seendevicesdatastore.repository.DevicePositionRepository
+import com.esmartit.seendevicesdatastore.repository.DeviceWithPosition
+import com.esmartit.seendevicesdatastore.repository.Position
+import org.springframework.stereotype.Service
+
+@Service
+class SeenDevicesPositionService(
+    private val eventToSensorActivity: EventToSensorActivity,
+    private val repository: DevicePositionRepository,
+    private val radiusActivity: RadiusActivityRepository
+) {
+
+    fun handle(event: DeviceWithPresenceEvent) {
+
+        val incomingSensorActivity = eventToSensorActivity.convertToSensorActivity(event.deviceDetectedEvent)
+        val existingSensorActivity = repository.findByMacAddressAndSeenTime(
+            incomingSensorActivity.device.macAddress,
+            incomingSensorActivity.seenTime
+        )
+
+        val existingRSSI = existingSensorActivity?.position ?: Position.NO_POSITION
+        val newPosition = if (event.position.value > existingRSSI.value) event.position else existingRSSI
+        val newCount = existingSensorActivity?.let { it.countInAnHour + 1 } ?: 1
+        val userInfo = radiusActivity.findByInfoCallingStationId(incomingSensorActivity.device.macAddress)
+
+        repository.save(
+            DeviceWithPosition(
+                id = existingSensorActivity?.id,
+                macAddress = incomingSensorActivity.device.macAddress,
+                position = newPosition,
+                seenTime = incomingSensorActivity.seenTime,
+                countInAnHour = newCount,
+                userInfo = userInfo
+            )
+        )
+    }
+}
