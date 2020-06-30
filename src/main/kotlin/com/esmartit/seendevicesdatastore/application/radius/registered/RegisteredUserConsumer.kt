c0492d2da5d6564898735e7467600264a96315f4
gus-esmartit <gustavo.liccioni@esmartit.es>
2020-06-30 23:34:40 +0200

    fix: adding log

--- src/main/kotlin/com/esmartit/seendevicesdatastore/application/radius/registered/RegisteredUserConsumer.kt
+++ src/main/kotlin/com/esmartit/seendevicesdatastore/application/radius/registered/RegisteredUserConsumer.kt
@@ -18,9 +18,15 @@ class RegisteredUserConsumer(
 
     @StreamListener(RegisteredUserInput.REGISTERED_DEVICE_INPUT)
     fun handle(event: RegisteredEvent) {
-        val registeredDevice = repository.findByInfoUsername(event.username)?.copy(info = event.toInfo())
-            ?: RegisteredUser(info = event.toInfo())
-        repository.save(registeredDevice)
+        try {
+            println(event)
+            val registeredDevice = repository.findByInfoUsername(event.username)?.copy(info = event.toInfo())
+                ?: RegisteredUser(info = event.toInfo())
+            repository.save(registeredDevice)
+            println(registeredDevice)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
 
