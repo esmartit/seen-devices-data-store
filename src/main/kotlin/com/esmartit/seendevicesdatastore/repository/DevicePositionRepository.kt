03d1741ea8e46e3a5d3d39765b300ea1f5b237b6
esmartit <61061163+esmartit@users.noreply.github.com>
2020-07-03 12:43:34 +0200

    feat: adding index t (#33)
    Co-authored-by: gus-esmartit <gustavo.liccioni@esmartit.es>

--- src/main/kotlin/com/esmartit/seendevicesdatastore/repository/DevicePositionRepository.kt
+++ src/main/kotlin/com/esmartit/seendevicesdatastore/repository/DevicePositionRepository.kt
@@ -1,6 +1,9 @@
 package com.esmartit.seendevicesdatastore.repository
 
 import com.esmartit.seendevicesdatastore.application.radius.online.RadiusActivity
+import org.springframework.data.mongodb.core.index.CompoundIndex
+import org.springframework.data.mongodb.core.index.CompoundIndexes
+import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
@@ -9,13 +12,16 @@ import reactor.core.publisher.Flux
 import java.time.Instant
 
 @Document
+@CompoundIndex(def = "{'macAddress':1, 'seenTime':1}", name = "device_with_position_macAddress_seenTime_idx")
 data class DeviceWithPosition(
     val id: String? = null,
     val macAddress: String,
+    @Indexed(name = "seenTime_idx")
     val seenTime: Instant,
     val position: Position,
     val countInAnHour: Int = 0,
     val userInfo: RadiusActivity? = null,
+    @Indexed(name = "lastUpdate_idx")
     val lastUpdate: Instant = seenTime
 ) {
     fun isWithinRange(): Boolean {
