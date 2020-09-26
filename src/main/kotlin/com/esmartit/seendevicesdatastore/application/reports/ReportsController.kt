1433929ba396b4b49115aac6384cf7a79f534c16
gus-esmartit <gustavo.liccioni@esmartit.es>
2020-09-26 19:58:24 +0200

    feat: reports list

--- src/main/kotlin/com/esmartit/seendevicesdatastore/application/reports/ReportsController.kt
+++ src/main/kotlin/com/esmartit/seendevicesdatastore/application/reports/ReportsController.kt
@@ -1,22 +1,27 @@
 package com.esmartit.seendevicesdatastore.application.reports
 
-import com.esmartit.seendevicesdatastore.domain.ScanApiActivity
 import com.esmartit.seendevicesdatastore.domain.FilterRequest
+import com.esmartit.seendevicesdatastore.domain.ScanApiActivity
+import com.esmartit.seendevicesdatastore.services.ScanApiService
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/reports")
-class ReportsController {
+class ReportsController(private val scanApiService: ScanApiService) {
 
-    @GetMapping(path = ["/find"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    @GetMapping(path = ["/list"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
     fun getDailyConnected(
-        requestFilters: FilterRequest
-    ): Flux<DeviceWithPositionRecord> {
-        TODO()
+        filters: FilterRequest
+    ): Flux<MutableList<DeviceWithPositionRecord>> {
+        return scanApiService.filteredFlux(filters)
+            .map { DeviceWithPositionRecord(it) }
+            .buffer(100)
+            .concatWith(Mono.just(listOf(DeviceWithPositionRecord(isLast = true))))
     }
 }
 
