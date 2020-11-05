2b037c3dc59c3cadbadfb5fa3a28cba44a2cd530
gus-esmartit <gustavo.liccioni@esmartit.es>
2020-11-05 21:45:59 +0100

    fix: adding resources requests

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,6 @@ EXPOSE 9000
 RUN mkdir /app
 
 COPY build/libs/*.jar /app/application.jar
-ENTRYPOINT ["java", "-XX:+UnlockExperimentalVMOptions", "-XX:+UseCGroupMemoryLimitForHeap", "-Djava.security.egd=file:/dev/./urandom","-jar", "/app/application.jar"]
+#ENTRYPOINT ["java", "-XX:+UnlockExperimentalVMOptions", "-XX:+UseCGroupMemoryLimitForHeap", "-Djava.security.egd=file:/dev/./urandom","-jar", "/app/application.jar"]
+ENV JAVA_OPTS="-Xmx1g -Xms512m -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Djava.security.egd=file:/dev/./urandom"
+ENTRYPOINT exec java $JAVA_OPTS -jar /app/application.jar
--- src/main/kotlin/com/esmartit/seendevicesdatastore/SeenDevicesDataStoreApplication.kt
+++ src/main/kotlin/com/esmartit/seendevicesdatastore/SeenDevicesDataStoreApplication.kt
@@ -1,15 +1,37 @@
 package com.esmartit.seendevicesdatastore
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
+import java.text.NumberFormat
 
 @SpringBootApplication
 @EnableReactiveMongoRepositories
 @EnableMongoRepositories
 class SeenDevicesDataStoreApplication
 
+private val logger = LoggerFactory.getLogger(SeenDevicesDataStoreApplication::class.java)
 fun main(args: Array<String>) {
-	runApplication<SeenDevicesDataStoreApplication>(*args)
+    runApplication<SeenDevicesDataStoreApplication>(*args)
+
+    val runtime = Runtime.getRuntime()
+
+    val format: NumberFormat = NumberFormat.getInstance()
+
+    val maxMemory = runtime.maxMemory()
+    val allocatedMemory = runtime.totalMemory()
+    val freeMemory = runtime.freeMemory()
+    val mb = 1024 * 1024
+    val mega = " MB"
+
+    logger.info("========================== Memory Info ==========================")
+    logger.info("Free memory: " + format.format(freeMemory / mb).toString() + mega)
+    logger.info("Allocated memory: " + format.format(allocatedMemory / mb).toString() + mega)
+    logger.info("Max memory: " + format.format(maxMemory / mb).toString() + mega)
+    logger.info(
+		"Total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / mb).toString() + mega
+	)
+    logger.info("=================================================================\n")
 }
