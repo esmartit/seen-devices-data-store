3842d5b31d9039a9f08e2f8b2d7751d48177ebf7
esmartit <61061163+esmartit@users.noreply.github.com>
2022-02-09 11:21:01 +0100

    fix: modify dateAtZone and timeZone on scanApiActivityH. (#91)
    Co-authored-by: AZignago <adolfo.zignago@esmartit.es>

--- src/main/kotlin/com/esmartit/seendevicesdatastore/application/scanapi/hourly/ScanApiHourlyReactiveRepository.kt
+++ src/main/kotlin/com/esmartit/seendevicesdatastore/application/scanapi/hourly/ScanApiHourlyReactiveRepository.kt
@@ -5,15 +5,16 @@ import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
+import java.time.Instant
 
 @Repository
 interface ScanApiHourlyReactiveRepository : ReactiveMongoRepository<ScanApiActivityH, String> {
-    fun findByDateAtZoneGreaterThanEqual(dateAtZone: String): Flux<ScanApiActivityH>
-    fun findByDateAtZoneBetween(startDateFilter: String, end: String): Flux<ScanApiActivityH>
-    fun findByDateAtZoneLessThanEqual(end: String): Flux<ScanApiActivityH>
+    fun findByDateAtZoneGreaterThanEqual(dateAtZone: Instant): Flux<ScanApiActivityH>
+    fun findByDateAtZoneBetween(startDateFilter: Instant, end: Instant): Flux<ScanApiActivityH>
+    fun findByDateAtZoneLessThanEqual(end: Instant): Flux<ScanApiActivityH>
 }
 
 @Repository
 interface ScanApiHpurlyRepository : MongoRepository<ScanApiActivityH, String> {
-    fun findByClientMacAndDateAtZone(clientMac: String, dateAtZone: String): ScanApiActivityH?
+    fun findByClientMacAndDateAtZone(clientMac: String, dateAtZone: Instant): ScanApiActivityH?
 }
